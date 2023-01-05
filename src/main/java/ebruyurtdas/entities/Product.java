1812070f95f7a74e4c2e57997635cac898c138ee
ebru <ebru_yurtdas@hotmail.com>
2023-01-05 19:58:46 +0300

    Product has been created. I will create Category class.

---
+++ src/main/java/ebruyurtdas/entities/Product.java
@@ -0,0 +1,75 @@
+package ebruyurtdas.entities;
+
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "description")
+    private String description;
+    
+    @Column(name = "image_url")
+    private String imageUrl;
+
+	public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", Name=" + productName + ",Price="+ price+",Image="+ imageUrl +", Description=" + description + "]";
+	}
+}
